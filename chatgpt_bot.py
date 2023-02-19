a3fa1469502ca6cf8a37fabe1e7f2983d0a24305
legojesus <yaronexp@gmail.com>
2023-02-19 18:08:39 +0200

    Added rule to ignore .idea folder

--- chatgpt_bot.py
+++ chatgpt_bot.py
@@ -1,12 +1,12 @@
-import openai   # OpenAI API for processing text and reply like a human.
-import os       # For getting the API key from environment variable.
+import openai  # OpenAI API for processing text and reply like a human.
+import os  # For getting the API key from environment variable.
 from dotenv import load_dotenv  # For getting the API key from .env file into the environment.
 
 load_dotenv()
 
 # Init OpenAI
 openai.api_key = os.getenv("API_KEY")
-model_engine = "text-davinci-003"   # The OpenAI GPT-3 model version. Reference: https://platform.openai.com/docs/models/gpt-3
+model_engine = "text-davinci-003"  # The OpenAI GPT-3 model version. Reference: https://platform.openai.com/docs/models/gpt-3
 
 
 def send_prompt_to_openai(prompt):
@@ -20,11 +20,13 @@ def send_prompt_to_openai(prompt):
             reply (string): The answer received from ChatGPT.
     """
     # Send prompt to ChatGPT and get possible replies. Reference: https://platform.openai.com/docs/api-reference/completions/create
-    completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1000, n=1, stop=None, temperature=0.7)
+    completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1000, n=1, stop=None,
+                                          temperature=0.3)
 
     # Get the first reply
-    reply = completion.choices[0].text
-    print(reply)
-    print("\n")
-
-    return reply
+    if completion is not None:
+        reply = completion.choices[0].text
+        print(reply)
+        print("\n")
+        return reply
+    return "No query sent"
