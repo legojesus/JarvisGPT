cb99eb25003d5e75275ea67683fdba0881228fde
legojesus <yaronexp@gmail.com>
2023-02-19 18:12:41 +0200

    Added different logic for different OSs since voices are very different between different OSs engines

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,14 +1,22 @@
-import pyttsx3  # Text to speech synthesizer.
+import platform     # Figure out the OS of the user
+import pyttsx3      # Text to speech synthesizer for Windows (can work on linux but voice sounds horrible).
+import subprocess   # Manually activate espeak on linux as it sounds better than pyttsx3.
 
+
+OS = platform.system()
 
 # Init pyttsx3 (Py Text-To-Speech)
 talkbot = pyttsx3.init()
-talkbot.setProperty('rate', 140)                # Setting the voice's talking speed
-talkbot.setProperty('volume',1.0)               # Setting the volume level between 0 and 1
-voices = talkbot.getProperty('voices')          # Getting details of current voice
-talkbot.setProperty('voice', voices[0].id)    # Set voice to male 
-# talkbot.setProperty('voice', voices[1].id)      # Set voice to female
+talkbot.setProperty('rate', 100)        # Setting the voice's talking speed
+talkbot.setProperty('volume', 1.0)      # Setting the volume level between 0 and 1
+voices = talkbot.getProperty('voices')  # Getting details of current voice
+talkbot.setProperty('voice', 0)         # 0 for male voice.
 
+# for voice in voices:
+#     talkbot.setProperty('voice', voice.id)
+#     print(voice)
+# talkbot.say('The quick brown fox jumped over the lazy dog.')
+# talkbot.runAndWait()
 
 
 def read_answer_to_user(answer):
@@ -21,7 +29,13 @@ def read_answer_to_user(answer):
         Returns:
             None.
     """
+    if OS == "Windows":
+        talkbot.say(answer)
+        talkbot.runAndWait()
+        talkbot.stop()
+    elif OS == "Linux":
+        subprocess.run(f"espeak -v mb-en1 -s 140 '{answer}'", shell=True,
+                       universal_newlines=True,
+                       capture_output=True,
+                       text=True)
 
-    talkbot.say(answer)
-    talkbot.runAndWait()
-    talkbot.stop()
